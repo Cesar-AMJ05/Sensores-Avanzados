2f80973e5dd62654f02c4452c5e830fb3f769e9d
Cesar Alberto Martinez Juarez <72173111+Cesar-AMJ05@users.noreply.github.com>
2023-10-30 22:38:55 -0600

    Delete MPU_2 directory

--- MPU_2/rule_simp13.m
+++
@@ -1,5 +0,0 @@
-function F= rule_simp13(t,omega)
-    h=(t(1,2)-t(1,1))/2;
-%F=(h/3)*(fo+4f1+f2);
-    F=(h/3)*(omega(1,1)+(4*omega(1,2))+omega(1,3));
-end
--- MPU_2/trapecio_intg.m
+++
@@ -1,5 +0,0 @@
-function F =trapecio_intg(t,omega)
-h=(t(1,2)-t(1,1));
-%F=(h/2)*(fo+f1);
-F=(h/2)*(omega(1,1)+omega(1,2));
-end
